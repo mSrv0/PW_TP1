b2893bb9721b2b1543f69e39e32b5355fa102004
Msoto <msoto@puenteweb.com>
2022-10-21 09:56:11 -0300

    Formulario Editar Clientes

--- menu/untEditClientes.pas
+++ menu/untEditClientes.pas
@@ -5,17 +5,28 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Controls.Presentation, FMX.Objects, FMX.Edit, cUnit;
+  FMX.Controls.Presentation, FMX.Objects, FMX.Edit, cUnit, FMX.Layouts;
 
 type
   TfmEditClientes = class(TForm)
-    btn_Editar: TButton;
-    Label1: TLabel;
-    img_BackCli: TImage;
-    Rectangle1: TRectangle;
-    Label2: TLabel;
-    ed_localidad: TEdit;
     lb_RazonSocial: TLabel;
+    lb_MasInfo: TLabel;
+    img_BackCli: TImage;
+    ly_ContEdits: TLayout;
+    StyleBook1: TStyleBook;
+    ed_Domicilio: TEdit;
+    lb_Domicilio: TLabel;
+    lb_Localidad: TLabel;
+    ed_Localidad: TEdit;
+    lb_Telefono: TLabel;
+    ed_Telefono: TEdit;
+    ed_Email: TEdit;
+    lb_Email: TLabel;
+    rec_ContBotones: TRectangle;
+    Rectangle1: TRectangle;
+    btn_Aceptar: TButton;
+    Rectangle2: TRectangle;
+    btn_Cancelar: TButton;
   private
     { Private declarations }
   public
